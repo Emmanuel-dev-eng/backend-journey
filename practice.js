678eb04c5e883b4e193e7d81397801d1d47e7ab4
Emmanuel-dev-eng <mrstories7777@gmail.com>
2026-09-11 10:33:50 +0100

    My first commit - practice.js with variables, loops, and functions

---
+++ practice.js
@@ -0,0 +1,35 @@
+let name = "EMMANUEL";
+let age = 20;
+
+age = age + 1; // we can change what's inside the box
+
+console.log('${name} is now ${age} years old ');
+for (let i= 0; i <= 9; i++){
+  if (i % 2 === 0){
+    console.log(i)
+  }
+}
+
+function isme(Number){
+  if (Number % 2 === 0){
+    return "even";
+  } else{
+    return"odd";
+  }
+}
+
+console.log(isme(19));
+console.log(isme(18));
+
+
+function add(nub , numbers){
+  return nub + numbers
+}
+
+console.log(add(1,9));
+
+function great(name){
+
+  return "hello ,"  + name + "!"
+}
+console.log(great("emmanuel"));
