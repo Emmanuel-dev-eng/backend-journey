2eea8f5f5a1b6260e4078f4bc1eb54bb5c2676e3
Emmanuel-dev-eng <mrstories7777@gmail.com>
2026-09-11 10:36:49 +0100

    mmmmm

--- practice.js
+++ practice.js
@@ -33,3 +33,10 @@ function great(name){
   return "hello ,"  + name + "!"
 }
 console.log(great("emmanuel"));
+console.log(add(1,9));
+
+function great(name){
+
+  return "hello ,"  + name + "!"
+}
+console.log(great("emmanuel"));
