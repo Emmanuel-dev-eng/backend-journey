ab3e4ec873ba36c2a44dd2c0cb43f750ff6ff5d2
Emmanuel-dev-eng <mrstories7777@gmail.com>
2026-09-16 11:53:16 +0100

    Phase 4: Express POST route reading req.body (name, age), tested with Postman

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('SERVER ACcTIVATED');
+  res.send('SERVER ACTIVATED');
 });
 
 app.get('/about', (req, res) => {
   res.send('THIS IS MY BACKEND LEARNING JOURNEY');
 });
+
 app.post('/hello', (req, res) => {
-  res.send('you just sent a post request');
+  console.log(req.body);
+  res.send(`Hello, ${req.body.name}! You are ${req.body.age} years old.`);
 });
 
 app.listen(3000, () => {
