6741918ebe5da288961c38874b3b9b6897773b49
Emmanuel-dev-eng <mrstories7777@gmail.com>
2026-09-17 19:37:34 +0100

    in proccess

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
-const express=require("express");
-const app =express();
+require('dotenv').config();  // 1st — always at the very top, before anything else
+
+const express = require('express');
+const app = express();
 
 app.use(express.json());
 
-app.get('/', (req , res) => {
-  res.send('welcome home');
+
+
+app.get('/', (req, res) => {
+  res.send('SERVER ACTIVATED');
 });
 
-app.get('/about', (req , res) => {
-  res.send('welcome home about');
+app.get('/about', (req, res) => {
+  res.send('THIS IS MY BACKEND LEARNING JOURNEY');
 });
 
-app.post('/hello' , (req , res) =>{
+app.post('/hello', (req, res) => {
   console.log(req.body);
-  res.send(`welcome MR  ${req.body.name} !`);
+  res.send(`Hello, ${req.body.name}! You are ${req.body.age} years old.`);
 });
 
-app.put('/hello' , (req , res) => {
+app.put('/hello', (req, res) => {
   console.log(req.body);
-  res.send(`WELCOME NAME UPDATED TO ${req.body.name}`);
+  res.send(`Updated! New name is ${req.body.name}`);
 });
-app.delete('/hello' , (req , res) =>{
-  res.send(`welcome to  ${req.body}`);
-} );
 
-app.listen(3000 ,() =>{
-  console.log('app running at http://localhost:3000');
+app.delete('/hello', (req, res) => {
+  res.send('Item deleted successfully');
+});
+
+app.listen(3000, () => {
+  console.log('server running on http://localhost:3000');
 });
